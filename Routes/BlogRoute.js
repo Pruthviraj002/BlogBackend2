b83651d2656f6211bcd3c812d9610633c5f13b83
pruthviraj Yadav <114584082+Pruthviraj002@users.noreply.github.com>
2024-11-24 13:02:20 +0530

    add middleware

--- Routes/BlogRoute.js
+++ Routes/BlogRoute.js
@@ -1,12 +1,13 @@
 const {getBlog,postBlog,putBlog,deleteBlog} = require("../Controller/BlogController")
 const { login } = require("../Controller/UserController")
+const auth = require("../middleware/auth")
 
 const route = require("express").Router()
 
 route.get("/",getBlog)
-route.post("/",postBlog)
-route.post("/login",login)
-route.put("/:id",putBlog)
-route.delete("/:id",deleteBlog)
+route.post("/",auth,postBlog)
+route.post("/login",auth,login)
+route.put("/:id",auth,putBlog)
+route.delete("/:id",auth,deleteBlog)
 
 module.exports = route 
